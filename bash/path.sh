4238823000feb911b1e723a3623dd1a443cfb6eb
John Mertz <git@john.me.tz>
2026-06-20 20:02:39 -0600

    Update and Export all default PERL5LIBS plus local

--- bash/path.sh
+++ bash/path.sh
@@ -1,3 +1,5 @@
-export PERL5LIB=$PWD/local/lib/perl5
+PERL5LIB=''
+PERL5LIB=$(perl -e 'print join ":",@INC')
+export PERL5LIB=$PWD/local/lib/perl5:$(echo $PERL5LIB | sed -r 's#^[^:]+/local/lib/perl5:##')
 export PATH=$PWD/local/bin:$(echo $PATH | sed -r 's#^[^:]+/local/bin:##')
 eval "$(perl -I $HOME/perl5/lib/perl5 -Mlocal::lib)"
